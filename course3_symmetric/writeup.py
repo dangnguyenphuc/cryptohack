57e9291638abef51334909cae39a6a98f532abdd
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2023-03-25 00:05:12 +0700

    Symmetric Cryptography - Round Keys:DONE

--- course3_symmetric/writeup.py
+++ course3_symmetric/writeup.py
@@ -1,5 +1,5 @@
-
-
+from pwn import *
+from Crypto.Util.number import *
 #  Keyed Permutations
 
 '''
@@ -30,11 +30,38 @@ def matrix2bytes(matrix):
 
     return res
 
-matrix = [
-    [99, 114, 121, 112],
-    [116, 111, 123, 105],
-    [110, 109, 97, 116],
-    [114, 105, 120, 125],
+# matrix = [
+#     [99, 114, 121, 112],
+#     [116, 111, 123, 105],
+#     [110, 109, 97, 116],
+#     [114, 105, 120, 125],
+# ]
+
+# print(matrix2bytes(matrix))
+
+# Round Keys
+state = [
+    [206, 243, 61, 34],
+    [171, 11, 93, 31],
+    [16, 200, 91, 108],
+    [150, 3, 194, 51],
 ]
 
-print(matrix2bytes(matrix))
+round_key = [
+    [173, 129, 68, 82],
+    [223, 100, 38, 109],
+    [32, 189, 53, 8],
+    [253, 48, 187, 78],
+]
+
+
+def add_round_key(s, k):
+    assert len(s)==len(k), "States and Key are not same size!!!"
+    key = []
+    for i in range(len(state)):
+        key.append([ bytes_to_long(xor(state[i][a], round_key[i][a])) for a in range(len(state[i]))])
+    return key
+
+
+cur_key = (add_round_key(state, round_key))
+print(matrix2bytes(cur_key))
