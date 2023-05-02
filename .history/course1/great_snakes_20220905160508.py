9054b8e53eb35aa80870c01e213e95522d2987e9
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2023-05-02 12:46:23 +0700

    update

--- .history/course1/great_snakes_20220905160508.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-# import this
-
-ords = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
-
-print("Here is your flag:")
-print("".join(chr(o) for o in ords))
--- .history/general/writeup_20230322204227.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3 -f
-
-
-def gcd(number1, number2):
-    if(number1 == 0): return number2
-    return gcd(number2%number1, number1)
-
-print(gcd(32, 44223))
