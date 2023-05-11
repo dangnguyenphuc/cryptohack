ca2145503e8f811267996642116766969e9dc919
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2023-05-11 15:50:36 +0700

    Public Key Course: Diffie-Hellman Starter 4:DONE

--- course4_publicKey/writeup.py
+++ course4_publicKey/writeup.py
@@ -306,29 +306,6 @@ def inv(number1, number2):
 # e = "0x9d0637faa46281b533e83cc37e1cf5626bd33f712cc1948622f10ec26f766fb37b9cd6c7a6e4b2c03bce0dd70d5a3a28b6b0c941d8792bc6a870568790ebcd30f40277af59e0fd3141e272c48f8e33592965997c7d93006c27bf3a2b8fb71831dfa939c0ba2c7569dd1b660efc6c8966e674fbe6e051811d92a802c789d895f356ceec9722d5a7b617d21b8aa42dd6a45de721953939a5a81b8dffc9490acd4f60b0c0475883ff7e2ab50b39b2deeedaefefffc52ae2e03f72756d9b4f7b6bd85b1a6764b31312bc375a2298b78b0263d492205d2a5aa7a227abaf41ab4ea8ce0e75728a5177fe90ace36fdc5dba53317bbf90e60a6f2311bb333bf55ba3245f"
 # c = "0xa3bce6e2e677d7855a1a7819eb1879779d1e1eefa21a1a6e205c8b46fdc020a2487fdd07dbae99274204fadda2ba69af73627bdddcb2c403118f507bca03cb0bad7a8cd03f70defc31fa904d71230aab98a10e155bf207da1b1cac1503f48cab3758024cc6e62afe99767e9e4c151b75f60d8f7989c152fdf4ff4b95ceed9a7065f38c68dee4dd0da503650d3246d463f504b36e1d6fafabb35d2390ecf0419b2bb67c4c647fb38511b34eb494d9289c872203fa70f4084d2fa2367a63a8881b74cc38730ad7584328de6a7d92e4ca18098a15119baee91237cea24975bdfc19bdbce7c1559899a88125935584cd37c8dd31f3f2b4517eefae84e7e588344fa5"
 
-
-# ------------------------------------------------------------------------------------------------------------
-#  Diffie-Hellman Starter 1
-# p = 991
-# g = 209
-# print(inv(g, p))
-
-
-# ------------------------------------------------------------------------------------------------------------
-#  Diffie-Hellman Starter 2
-'''We just need to detect Cycle :))'''
-p = 28151
-
-found = False
-for i in range(2,p):
-    found = True
-    for j in range(2,p):
-        a = pow(i, j, p)
-        if a == i:
-            found = False
-            break
-    if found:
-        print("Found: " + str(i))
         
 
 
