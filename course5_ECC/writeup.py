9b4148757e76acbcf950af2426af91fba42a3f28
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2023-06-03 14:17:52 +0700

    Course 5: Point Addition - Done

--- course5_ECC/writeup.py
+++ course5_ECC/writeup.py
@@ -1,3 +1,36 @@
+# extended Euclide
+def extended_gcd(a,b):
+    s = 0
+    old_s = 1
+    r = b
+    old_r = a
+
+    bezout_t = None
+
+    q = None
+    while r!= 0:
+        q = int(old_r / r)
+        old_r, r = r, old_r - q*r
+        old_s, s = s, old_s - q*s
+
+    if b!=0 :
+        bezout_t = int((old_r - old_s*a) / b)
+    else: 
+        bezout_t = 0
+    
+    # print(old_s, bezout_t)
+    # print("GCD: " + str(old_r))
+    return (old_s,bezout_t,old_r)
+
+def inv(number1, number2):
+    if number1 < 0: number1 += number2
+    (old_s,bezout_t,old_r) = extended_gcd(number1, number2)
+    if old_r > 1: return None
+    if old_s < 0: old_s+=number2
+    return old_s
+
+
+
 # ------------------------------------------------------------------------------------------------------------
 # Point Negation
 # E: Y2 = X3 + 497 X + 1768, p: 9739 
@@ -6,11 +39,45 @@
 https://trustica.cz/2018/03/08/elliptic-curves-point-negation/
 −Py mod p = p−Py
 '''
-p = 9739
-A = (8045,6936)
-A_neg = (A[0], p-A[1])
-print(A_neg)
+# p = 9739
+# A = (8045,6936)
+# A_neg = (A[0], p-A[1])
+# print(A_neg)
 # (8045, 2803)
 # ------------------------------------------------------------------------------------------------------------
 #  Point Addition
+'''
+Algorithm for the addition of two points: P + Q
 
+(a) If P = O, then P + Q = Q.
+(b) Otherwise, if Q = O, then P + Q = P.
+(c) Otherwise, write P = (x1, y1) and Q = (x2, y2).
+(d) If x1 = x2 and y1 = −y2, then P + Q = O.
+(e) Otherwise:
+  (e1) if P ≠ Q: λ = (y2 - y1) / (x2 - x1)
+  (e2) if P = Q: λ = (3x12 + a) / 2y1
+(f) x3 = λ2 − x1 − x2,     y3 = λ(x1 −x3) − y1
+(g) P + Q = (x3, y3) 
+'''
+A = 497
+B = 1768
+MOD = 9739
+def pointAddition(p, q, a = A, b = B, mod = MOD):
+    if p == (0,0): return q
+    if q == (0,0): return p
+    if p[0] == q[0] and ((p[1] + q[1])%mod == 0): return (0,0)
+    if p != q: lam = (q[1]-p[1])*inv(q[0]-p[0], mod)%mod 
+    else: lam = (3*(p[0]**2) + a)*inv(2*p[1], mod)%mod
+    
+    x3 = (lam**2 - p[0] - q[0])%mod
+    y3 = (lam*(p[0]-x3) - p[1])%mod
+    return (x3,y3)
+
+P = (493, 5564)
+Q = (1539, 4742) 
+R = (4403,5202)
+
+res = pointAddition(pointAddition(pointAddition(P,P),Q),R)
+print(res)
+
+# ------------------------------------------------------------------------------------------------------------
